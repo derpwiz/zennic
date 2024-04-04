017b8fa2379aa17a46edb5f62926494b699a413a
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-04 15:51:50 -0400

    Split UtilityAreaTerminalView into smaller components

---
+++ zennic/Models/Terminal/TerminalHistory.swift
@@ -0,0 +1,20 @@
+//
+// TerminalHistory.swift
+// Zennic
+//
+// Created on 2024-04-04 10:02:20
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalHistory {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
