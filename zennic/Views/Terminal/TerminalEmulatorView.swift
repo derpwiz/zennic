dbc05e8a9e0e8740d01ab7491ad5c0d57a6f03b8
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-06 10:45:11 -0400

    Split large functions in GitWrapper into smaller files

---
+++ zennic/Views/Terminal/TerminalEmulatorView.swift
@@ -0,0 +1,20 @@
+//
+// TerminalEmulatorView.swift
+// Zennic
+//
+// Created on 2024-04-06 10:45:11
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalEmulatorView {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
