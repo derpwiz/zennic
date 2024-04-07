be76170d1ad774c4a6091b2935c70d9d7b1715de
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-07 16:31:48 -0400

    Optimize Swift compile time with modularization

---
+++ zennic/Views/Terminal/TerminalPromptView.swift
@@ -0,0 +1,20 @@
+//
+// TerminalPromptView.swift
+// Zennic
+//
+// Created on 2024-04-07 12:05:29
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalPromptView {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
