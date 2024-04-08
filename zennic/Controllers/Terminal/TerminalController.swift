f1b73ec5703363daedc319486094f2042a1f08b1
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-08 13:05:36 -0400

    Update Swift syntax to Swift 5.8 standards

---
+++ zennic/Controllers/Terminal/TerminalController.swift
@@ -0,0 +1,20 @@
+//
+// TerminalController.swift
+// Zennic
+//
+// Created on 2024-04-08 13:05:36
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalController {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
