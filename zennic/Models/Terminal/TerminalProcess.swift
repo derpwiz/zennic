cb4b86869aef675f1dcf137276d310a2759cc89a
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-11 11:38:59 -0400

    Fix terminal history navigation

---
+++ zennic/Models/Terminal/TerminalProcess.swift
@@ -0,0 +1,20 @@
+//
+// TerminalProcess.swift
+// Zennic
+//
+// Created on 2024-04-11 11:38:59
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalProcess {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
