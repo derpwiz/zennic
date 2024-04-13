78ee342dc76bb00a337eddb1f647c5352b53081b
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-13 16:06:42 -0400

    Split UtilityAreaTerminalView into smaller components

---
+++ zennic/Utilities/Terminal/TerminalUtilities.swift
@@ -0,0 +1,20 @@
+//
+// TerminalUtilities.swift
+// Zennic
+//
+// Created on 2024-04-13 20:01:28
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalUtilities {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
