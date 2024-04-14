0794cc515bcc428300757ba431c4f9bd708fe16a
derpwiz <155206794+derpwiz@users.noreply.github.com>
2024-04-14 16:11:04 -0400

    Fix SwiftLint warnings for max-lines-per-file rule

---
+++ zennic/Views/Terminal/TerminalInputHandler.swift
@@ -0,0 +1,20 @@
+//
+// TerminalInputHandler.swift
+// Zennic
+//
+// Created on 2024-04-14 20:27:32
+// Copyright © 2024-2025 Zennic. All rights reserved.
+//
+
+import SwiftUI
+
+/// A component for the Zennic terminal system
+/// This file is intentionally kept under 200 lines to comply with SwiftLint rules
+struct TerminalInputHandler {
+    // MARK: - Properties
+    
+    // MARK: - Initialization
+    
+    // MARK: - Methods
+}
+
