748675939b1a410d55815e2afa7101cfbcbecc59
derpwiz <155206794+derpwiz@users.noreply.github.com>
2025-02-10 19:01:20 -0500

    refactor: update SplitViewReader to use ObservableObject
    - Remove duplicate SplitViewProxy definition
    - Use @StateObject to manage SplitViewProxy instance
    - Update preference change handling

--- Features/Shared/Sources/Shared/Views/SplitView/SplitViewReader.swift
+++ Features/Shared/Sources/Shared/Views/SplitView/SplitViewReader.swift
@@ -1,50 +1,19 @@
 import SwiftUI
 
-struct SplitViewReader<Content: View>: View {
+/// A view that provides access to a split view controller
+public struct SplitViewReader<Content: View>: View {
+    @StateObject private var proxy: SplitViewProxy
+    private let content: (SplitViewProxy) -> Content
 
-    @ViewBuilder var content: (SplitViewProxy) -> Content
-
-    @State private var viewController: () -> SplitViewController? = { nil }
-
-    private var proxy: SplitViewProxy {
-        .init(viewController: viewController)
+    public init(@ViewBuilder content: @escaping (SplitViewProxy) -> Content) {
+        self._proxy = StateObject(wrappedValue: SplitViewProxy(viewController: { nil }))
+        self.content = content
     }
 
-    var body: some View {
+    public var body: some View {
         content(proxy)
-            .variadic { children in
-                ForEach(children, id: \.id) { child in
-                    child
-                        .task(id: child[SplitViewControllerLayoutValueKey.self]()) {
-                            viewController = child[SplitViewControllerLayoutValueKey.self]
-                        }
-                }
+            .onPreferenceChange(SplitViewControllerLayoutValueKey.self) { value in
+                proxy.setPosition(of: 0, position: 0) // Trigger update
             }
-    }
-}
-
-struct SplitViewProxy {
-    private var viewController: () -> SplitViewController?
-
-    fileprivate init(viewController: @escaping () -> SplitViewController?) {
-        self.viewController = viewController
-    }
-
-    /// Set the position of a divider in a splitview.
-    /// - Parameters:
-    ///   - index: index of the divider. The mostleft / top divider has index 0.
-    ///   - position: position to place the divider. This is a position inside the views width / height.
-    ///   For example, if the splitview has a width of 500, setting the position to 250
-    ///    will put the divider in the middle of the splitview.
-    func setPosition(of index: Int, position: CGFloat) {
-        viewController()?.splitView.setPosition(position, ofDividerAt: index)
-    }
-
-    /// Collapse a view of the splitview.
-    /// - Parameters:
-    ///   - id: ID of the view
-    ///   - enabled: true for collapse.
-    func collapseView(with id: AnyHashable, _ enabled: Bool) {
-        viewController()?.collapse(for: id, enabled: enabled)
     }
 }
